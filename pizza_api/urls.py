ac122e624e04b72bff8bd79fcd5e726408ad076b
imran6932 <imransxcr53@gmail.com>
2021-10-17 02:04:32 +0530

    bug fixed

--- pizza_api/urls.py
+++ pizza_api/urls.py
@@ -5,8 +5,8 @@ from pizza_api import views
 
 
 urlpatterns = [
-    path('create/', views.PizzaCreateView.as_view()), # create url for pizza api
-    path('list/', views.PizzaListView.as_view()), # list url for pizza api
-    path('edit-delete/<int:pk>/', views.PizzaUpdateDeleteViews.as_view()), # update and delete url for pizza api
+    path('create/', views.PizzaCreateView.as_view()), # create endpoint for pizza api
+    path('list/', views.PizzaListView.as_view()), # list endpoint for pizza api
+    path('edit-delete/<int:pk>/', views.PizzaUpdateDeleteViews.as_view()), # update and delete endpoint for pizza api
 
 ]
